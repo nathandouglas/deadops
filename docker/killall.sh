b756c80da13bb09c0193ef4e8948c672f4de5b1d
Nathan Douglas <nathan.douglas@blueridgedynamics.com>
2017-09-18 17:46:56 -0400

    curl update script

---
+++ docker/killall.sh
@@ -0,0 +1,4 @@
+# Credit: https://gist.github.com/evanscottgray/8571828
+
+#!/bin/bash
+docker ps | awk {' print $1 '} | tail -n+2 > tmp.txt; for line in $(cat tmp.txt); do docker kill $line; done; rm tmp.txt
